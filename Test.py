881d5ae21da71dffd8c049b2ba5d569285a83b2f
AhmedSelim13 <40352595+AhmedSelim13@users.noreply.github.com>
2018-06-22 15:14:35 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,14 @@
 your_number = input("Please insert your number for test: ")
 is_not = 0
-c = int(your_number ** (1/2)) + 1
+c = int(your_number ** (1/2))
 if x % 2 == 0:
-— z = 1
+— is_not = 1
 — break
 for y in range(3,c,2):
 — if x % y == 0:
-— — z = 1
+— — is_not = 1
 — — break
-if z == 1 and x > 2:
+if is_not == 1 and x > 2:
 — print(str(your_number) + " is not prime.")
 else:
 — print(str(your_number) + "is prime.")
